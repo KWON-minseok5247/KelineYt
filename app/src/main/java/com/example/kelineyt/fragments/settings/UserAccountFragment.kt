5f99aeaf6dba04c90d5782ecdf19a0239485013d
kwonMinseok <kwondh5247@naver.com>
2024-01-04 14:25:51 +0900

    2024-01-04 14:25 UserAccountFragment 구현 완료 이미지 업로드 속도 단축, 내용 변경시 Dialog 나오도록 개선 Forgot Password 클릭 가능

--- app/src/main/java/com/example/kelineyt/fragments/settings/UserAccountFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/settings/UserAccountFragment.kt
@@ -1,16 +1,23 @@
 package com.example.kelineyt.fragments.settings
 
+import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -26,10 +33,16 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class UserAccountFragment: Fragment()  {
+class UserAccountFragment : Fragment() {
 
     lateinit var binding: FragmentUserAccountBinding
     private val viewModel by viewModels<UserAccountViewModels>()
+    private val GALLERY = 1
+    private var imageData: Uri? = null
+    lateinit var oldUser: User
+
+    private lateinit var callback: OnBackPressedCallback
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +52,7 @@ class UserAccountFragment: Fragment()  {
         binding = FragmentUserAccountBinding.inflate(layoutInflater)
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -52,6 +66,8 @@ class UserAccountFragment: Fragment()  {
                     is Resource.Success -> {
                         hideUserLoading()
                         enterData(it.data!!)
+                        oldUser = it.data
+                        Log.e("old user", oldUser.toString())
                     }
                     is Resource.Error -> {
                         hideUserLoading()
@@ -64,12 +80,115 @@ class UserAccountFragment: Fragment()  {
             }
         }
 
-        binding.imageEdit.setOnClickListener {
+        lifecycleScope.launchWhenStarted {
+            viewModel.updateUser.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonSave.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.buttonSave.revertAnimation()
+                        Toast.makeText(requireContext(), "저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    is Resource.Error -> {
+                        binding.buttonSave.revertAnimation()
+                        Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    else -> Unit
+
+                }
+            }
+        }
+
+        binding.buttonSave.setOnClickListener {
+            val edFirstName = binding.edFirstName.text.toString()
+            val edLastName = binding.edLastName.text.toString()
+            val edEmail = binding.edEmail.text.toString()
+            if (imageData == null) {
+                val user = User(edFirstName, edLastName, edEmail, oldUser.imagePath)
+//                user.copy(imagePath = )
+                viewModel.saveUser(user)
+            } else {
+                val user = User(edFirstName, edLastName, edEmail)
+                viewModel.saveUserWithImage(user, imageData!!)
+
+            }
+
+        }
+        binding.imageCloseUserAccount.setOnClickListener {
+            backPress()
 
         }
 
+
+        // 갤러리 여는 방법
+//https://believecom.tistory.com/722
+
+        binding.imageUser.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, GALLERY)
+        }
+
+        binding.imageEdit.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, GALLERY)
+        }
+
+        binding.tvUpdatePassword.setOnClickListener {
+            this.setupBottomSheetDialog {
+
+            }
+        }
+
+
+
+
     }
-        private fun hideUserLoading() {
+
+    private fun backPress() {
+        val edFirstName = binding.edFirstName.text.toString()
+        val edLastName = binding.edLastName.text.toString()
+        if (oldUser.firstName == edFirstName &&
+            oldUser.lastName == edLastName &&
+            imageData == null
+        ) {
+            findNavController().navigateUp()
+        } else {
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("저장이 되지 않았습니다.")
+                .setMessage("그래도 나가시겠습니까?")
+                .setPositiveButton("확인",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        findNavController().navigateUp()
+                    })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener { dialog, id ->
+                    })
+            builder.show()
+        }
+    }
+
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                imageData = data?.data
+                try {
+                    binding.imageUser.setImageURI(imageData)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun hideUserLoading() {
         binding.apply {
             progressbarAccount.visibility = View.GONE
             imageUser.visibility = View.VISIBLE
@@ -79,7 +198,8 @@ class UserAccountFragment: Fragment()  {
             edEmail.visibility = View.VISIBLE
             tvUpdatePassword.visibility = View.VISIBLE
             buttonSave.visibility = View.VISIBLE
-        }    }
+        }
+    }
 
     private fun showUserLoading() {
         binding.apply {
@@ -94,9 +214,27 @@ class UserAccountFragment: Fragment()  {
         }
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // 뒤로가기 클릭시 동작하는 로직
+                backPress()
+//                requireActivity().supportFragmentManager.beginTransaction()
+//                    .setCustomAnimations(0, R.anim.horizon_exit_front)
+//                    .remove(this@CartFragment)
+//                    .commit()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+
+
 
     private fun enterData(user: User) {
-        Glide.with(requireView()).load(user.imagePath).into(binding.imageUser)
+        Glide.with(this).load(user.imagePath).into(binding.imageUser)
         binding.apply {
             edFirstName.setText(user.firstName)
             edLastName.setText(user.lastName)
--- app/src/main/java/com/example/kelineyt/viewmodel/makeIt/UserAccountViewModels.kt
+++ app/src/main/java/com/example/kelineyt/viewmodel/makeIt/UserAccountViewModels.kt
@@ -1,24 +1,39 @@
 package com.example.kelineyt.viewmodel.makeIt
 
+import android.app.Application
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.kelineyt.KelineApplication
 import com.example.kelineyt.data.User
 import com.example.kelineyt.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
+import java.util.*
 import javax.inject.Inject
 
 
 @HiltViewModel
 class UserAccountViewModels @Inject constructor(
     private val auth: FirebaseAuth,
-    private val firestore: FirebaseFirestore
-) : ViewModel() {
+    private val firestore: FirebaseFirestore,
+    private val storage: StorageReference,
+    application: Application
+) : AndroidViewModel(application) {
     //TODO 여기서는 일단 접속을 함과 동시에 파이어베이스의 데이터를 불러오고 작성할 수 있어야 한다.
     // 그리고 데이터를 수정했으면 다시 파이어베이스에 업로드를 해야 함.
     // 마지막으로 사진을 업로드하고 새로 수정하는 역할도 있어야 한다.
@@ -26,17 +41,18 @@ class UserAccountViewModels @Inject constructor(
     private val _user = MutableStateFlow<Resource<User>>(Resource.unspecified())
     val user = _user.asStateFlow()
 
+    private val _updateUser = MutableStateFlow<Resource<User>>(Resource.unspecified())
+    val updateUser = _updateUser.asStateFlow()
+
     init {
         getUserData()
     }
 
 
     private fun getUserData() {
-
         viewModelScope.launch {
             _user.emit(Resource.Loading())
         }
-
         firestore.collection("user").document(auth.uid!!).get()
             .addOnSuccessListener {
                 val user = it.toObject(User::class.java)
@@ -51,19 +67,107 @@ class UserAccountViewModels @Inject constructor(
             }
     }
 
-    fun updateUserData(updateUser: User) {
+    fun saveUser(updateUser: User) { // 사진말고 닉네임만 변경시 작동
+
         viewModelScope.launch {
-            _user.emit(Resource.Loading())
+            _updateUser.emit(Resource.Loading())
         }
+
         firestore.collection("user").document(auth.uid!!).set(updateUser)
             .addOnSuccessListener {
                 viewModelScope.launch {
-                    _user.emit(Resource.Success(updateUser))
+                    _updateUser.emit(Resource.Success(updateUser))
                 }
             }.addOnFailureListener {
                 viewModelScope.launch {
-                    _user.emit(Resource.Error(it.message.toString()))
+                    _updateUser.emit(Resource.Error(it.message.toString()))
                 }
             }
+    }
+
+    fun saveUserWithImage(updateUser: User, uri: Uri) { // 사진만 혹은 사진과 같이 변경할 때 사용
+// 근데 속도가 너무 느린데?????????
+        viewModelScope.launch {
+            _updateUser.emit(Resource.Loading())
+        }
+        // 따라하니까 속도 2배는 빨라짐
+        viewModelScope.launch {
+            try {
+                val imageBitmap = MediaStore.Images.Media.getBitmap(getApplication<KelineApplication>()
+                    .contentResolver,uri)
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 96, byteArrayOutputStream)
+                val imageByteArray = byteArrayOutputStream.toByteArray()
+                // 어디에 저장할지 주소를 만들어두고
+                val imageDirectory = storage.child("profileImages/${auth.uid}/${UUID.randomUUID()}")
+                // 해당 주소에 데이터를 입력한다.
+                val result = imageDirectory.putBytes(imageByteArray).await()
+                val imageUrl = result.storage.downloadUrl.await().toString()
+                saveUser(updateUser.copy(imagePath = imageUrl))
+            } catch (e: Exception) {
+                viewModelScope.launch {
+                    _updateUser.emit(Resource.Error(e.message.toString()))
+                }
+            }
+        }
+//
+//        viewModelScope.launch {
+//            _updateUser.emit(Resource.Loading())
+//        }
+//        val fileName = "${System.currentTimeMillis()}.png"
+//
+//        storage.child("profileImages").child(auth.uid!!).child(fileName)
+//            .putFile(uri)
+//            .addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    // 파일 업로드에 성공했기 때문에 파일을 다시 받아 오도록 해야함
+//                    viewModelScope.launch {
+//                        val imageUrl = storage.child("profileImages").child(auth.uid!!)
+//                            .child(fileName).downloadUrl.await().toString()
+//
+//                        Log.e("imageurl", imageUrl)
+//                        val newUser = updateUser
+//                        saveUser(newUser.copy(imagePath = imageUrl))
+//                    }
+//
+//
+//                } else {
+//                    viewModelScope.launch {
+//                        _updateUser.emit(Resource.Error("사진 업로드 실패"))
+//                    }
+//                }
+//            }
+
+//        storage.child("profileImages").child(auth.uid!!).child(fileName)
+//            .putFile(uri)
+//            .addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    // 파일 업로드에 성공했기 때문에 파일을 다시 받아 오도록 해야함
+//                    storage.child("profileImages").child(auth.uid!!)
+//                        .child(fileName).downloadUrl
+//                        .addOnSuccessListener { uri -> // 여기가 문제야
+//
+//                            viewModelScope.launch {
+//                                val a = storage.child("profileImages").child(auth.uid!!)
+//                                    .child(fileName).downloadUrl.await().path
+//
+//                            }
+//
+//                            val newUser = updateUser
+//
+//                            newUser.copy(imagePath = uri.path.toString())
+//                            saveUser(newUser)
+//                        }.addOnFailureListener {
+//                            viewModelScope.launch {
+//                                _updateUser.emit(Resource.Error(it.message.toString()))
+//                            }
+//                        }
+//                } else {
+//                    viewModelScope.launch {
+//                        _updateUser.emit(Resource.Error("사진 업로드 실패"))
+//                    }
+//                }
+//            }
+
     }
 }
